89b5b76b44a270df93311d94b5f37daa2ac11607
jsanchco <jsanchco@gmail.com>
2021-03-14 20:07:24 +0100

    .

---
+++ src/API/Basic/Console.Basic/ConsoleApp.cs
@@ -0,0 +1,71 @@
+﻿using ConsoleApp.Basic.Models;
+using ConsoleApp.Basic.Services;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Basic
+{
+    public class ConsoleApp : IHostedService
+    {
+        private readonly Configuration<AppConfig> _configuration;
+        private readonly ILogger<ConsoleApp> _logger;
+
+        public ConsoleApp(
+            Configuration<AppConfig> configuration,
+            ILogger<ConsoleApp> logger)
+        {
+            _configuration = configuration;
+            _logger = logger;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            PrintTitle();
+            await Process();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        private void PrintTitle()
+        {
+            var lengthTotal = _configuration.Settings.Title.Length + 20;
+            Console.WriteLine($"{new string('*', lengthTotal)}");
+            Console.WriteLine($"*         {_configuration.Settings.Title}         *");
+            Console.WriteLine($"{new string('*', lengthTotal)}");
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Press (F1) to Test Authentication HMAC.");
+            Console.WriteLine("Press (ESC) to exit");
+            Console.WriteLine("");
+        }
+
+        private async Task Process()
+        {
+            ConsoleKey key;
+            do
+            {
+                while (!Console.KeyAvailable)
+                {
+                    // Do something, but don't read key here
+                }
+
+                // Key is available - read it
+                key = Console.ReadKey(true).Key;
+                switch (key)
+                {
+                    case ConsoleKey.F1:
+                        Console.WriteLine("Pressed F1 ...");
+                        _logger.LogInformation("Pressed F1 ...");
+                        break;
+                }
+            } while (key != ConsoleKey.Escape);
+        }
+    }
+}
---
+++ src/API/Basic/Console.Basic/Services/Configuration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace ConsoleApp.Basic.Services
+{
+    public class Configuration<T> where T : new()
+    {
+        public T Settings;
+
+        public Configuration()
+        {
+            Settings = InitOptions();
+        }
+
+        private static T InitOptions()
+        {
+            var config = InitConfig();
+            return config.Get<T>();
+        }
+
+        private static IConfigurationRoot InitConfig()
+        {
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile($"appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{env}.json", true, true)
+                .AddEnvironmentVariables();
+
+            return builder.Build();
+        }
+    }
+}
